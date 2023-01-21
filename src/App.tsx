e8eda9e425f1d38c9fed07da41f312c9980e1b43
kratzmendes <kratzmendes@gmail.com>
2023-01-20 22:47:56 -0300

    fix: push testing

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+         Hello World from Antecipation App
         </p>
         <a
           className="App-link"
